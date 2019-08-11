0a1b7754941df5ecf0bd2b12ff64f638741c7c16
MaryVorontsov <vorontsova_mi@soshace.com>
2019-08-11 11:24:16 +0300

    animation frames

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,16 @@
 var button = document.getElementById('left-menu-control');
 var menu = document.getElementById('left-menu');
+var openFlag = false;
 button.onclick = function() {
+	openFlag = !openFlag
 	menu.classList.toggle('active');
 	button.classList.toggle('active');
+	if (openFlag) {
+		button.innerHTML = 'close';
+	} 
+	else {
+
+		button.innerHTML = 'open';
+	}
+
 } 
