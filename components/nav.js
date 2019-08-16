33586df38f5b6c24d1dfb043232c7df7834f536d
MaryVorontsov <vorontsova_mi@soshace.com>
2019-08-16 17:19:01 +0300

    font awesome buttons

--- components/nav.js
+++ components/nav.js
@@ -2,15 +2,15 @@ var button = document.getElementById('left-menu-control');
 var menu = document.getElementById('left-menu');
 var openFlag = false;
 button.onclick = function() {
-	openFlag = !openFlag
+	openFlag = !openFlag;
 	menu.classList.toggle('active');
 	button.classList.toggle('active');
 	if (openFlag) {
-		button.innerHTML = 'close';
+		button.innerHTML = '<i class="far fa-window-close"></i>';
 	} 
 	else {
 
-		button.innerHTML = 'open';
+		button.innerHTML = '<i class="fas fa-bars"></i>';
 	}
 
 } 
